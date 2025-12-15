ad3c07f34839d715ecf12cfdbd4d5a441b81caff
Thomas <thomasvu.tech@gmail.com>
2025-12-15 17:47:36 -0500

    Update Node for linux

--- markdown-app/src/main/index.ts
+++ markdown-app/src/main/index.ts
@@ -13,11 +13,14 @@ function createWindow(): void {
     ...(process.platform === 'linux' ? { icon } : {}),
     center: true,
     title: 'NoteMark',
-    frame: false,
-    vibrancy: 'under-window',
-    visualEffectState: 'active',
-    titleBarStyle: 'hidden',
-    trafficLightPosition: { x: 15, y: 10 },
+    frame: true,
+    // THESE SETTINGS ARE MAC ONLY
+    // ---------------------------
+    //
+    // vibrancy: 'under-window',
+    // visualEffectState: 'active',
+    // titleBarStyle: 'hidden',
+    // trafficLightPosition: { x: 15, y: 10 },
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: true,
