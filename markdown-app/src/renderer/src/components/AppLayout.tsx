ffd5262882f69427726c5f88cb11b1cce1547751
ThomasNVu <139417064+ThomasNVu@users.noreply.github.com>
2025-12-15 17:45:25 -0500

    Create Sidebar

---
+++ markdown-app/src/renderer/src/components/AppLayout.tsx
@@ -0,0 +1,10 @@
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export const Sidebar = ({ className, children, ...props }: ComponentProps<'aside'>) => {
+  return (
+    <aside className={twMerge('w-62.5 mt-10 h-[100vh + 10px] overflow-auto', className)} {...props}>
+      {children}
+    </aside>
+  )
+}
