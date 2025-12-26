d8863658c8b0394e1ea7bbded1d5a69a4ea97f40
ThomasNVu <139417064+ThomasNVu@users.noreply.github.com>
2025-12-26 14:31:59 -0500

    Create Components for Sidebar

--- markdown-app/src/renderer/src/components/AppLayout.tsx
+++ markdown-app/src/renderer/src/components/AppLayout.tsx
@@ -1,5 +1,14 @@
-import { ComponentProps } from 'react'
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { ComponentProps, forwardRef } from 'react'
 import { twMerge } from 'tailwind-merge'
+
+export const RootLayout = ({ children, className, ...props }: ComponentProps<'main'>) => {
+  return (
+    <main className={twMerge('flex flex-row h-screen', className)} {...props}>
+      {children}
+    </main>
+  )
+}
 
 export const Sidebar = ({ className, children, ...props }: ComponentProps<'aside'>) => {
   return (
@@ -8,3 +17,13 @@ export const Sidebar = ({ className, children, ...props }: ComponentProps<'aside
     </aside>
   )
 }
+
+export const Content = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
+  ({ children, className, ...props }, ref) => (
+    <div ref={ref} className={twMerge('flex-1 overflow-auto', className)} {...props}>
+      {children}
+    </div>
+  )
+)
+
+Content.displayName = 'content'
