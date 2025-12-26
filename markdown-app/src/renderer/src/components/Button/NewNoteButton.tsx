221609300994bf2b80e312e3ca15a4d3b5c7c2f6
ThomasNVu <139417064+ThomasNVu@users.noreply.github.com>
2025-12-26 14:36:32 -0500

    Create buttons

---
+++ markdown-app/src/renderer/src/components/Button/NewNoteButton.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { ActionButton, ActionButtonProps } from '@/components'
+import { LuFile } from 'react-icons/lu'
+
+export const NewNoteButton = ({ ...props }: ActionButtonProps) => {
+  return (
+    <ActionButton {...props}>
+      <LuFile className="w-4 h-4 text-zinc-300" />
+    </ActionButton>
+  )
+}
