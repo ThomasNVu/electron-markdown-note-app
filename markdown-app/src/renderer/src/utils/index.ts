990d80f71ca46d3c972ce80ab87cdb5c5682ff8e
ThomasNVu <139417064+ThomasNVu@users.noreply.github.com>
2025-12-26 14:37:37 -0500

    Define Locale and time format

--- markdown-app/src/renderer/src/utils/index.ts
+++ markdown-app/src/renderer/src/utils/index.ts
@@ -1,6 +1,17 @@
 import clsx, { ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export const cd = (...args: ClassValue[]): string => {
+const locale = window.context?.locale ?? navigator.language
+
+const dateFormatter = new Intl.DateTimeFormat(locale, {
+  dateStyle: 'short',
+  timeStyle: 'short',
+  timeZone: 'UTC'
+})
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const formatDateFromMs = (ms: number) => dateFormatter.format(ms)
+
+export const cn = (...args: ClassValue[]): string => {
   return twMerge(clsx(...args))
 }
